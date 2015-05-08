05e65287ec629d17e6bc6cd09f493fb22fbbd514
Ken W. Alger <kenalger@comcast.net>
2015-05-07 18:29:38 -0700

    Continuing on...

--- leaderboard.py
+++ leaderboard.py
@@ -1,8 +1,10 @@
+"""This will create the Treehouse Leaderboard Application"""
+
 __author__ = "Ken W. Alger, David Dinkins, Dan Johnson, Keri Nicole"
 __copyright__ = "Copyright 2015, ZyzzyxTech"
 __credits__ = ["Ken W. Alger, Dan Johnson, David Dinkins, Keri Nicole"]
 __license__ = "GPL"
-__version__ = "1.0.1"
+__version__ = "0.1.0"
 __maintainer__ = "Ken W. Alger"
 __email__ = "ken@kenwalger.com"
 __status__ = "Development"
--- models.py
+++ models.py
@@ -1,14 +1,19 @@
+
+"""Handles the database related functions for the application."""
+
 __author__ = "Ken W. Alger, David Dinkins, Dan Johnson,  Keri Nicole"
 __copyright__ = "Copyright 2015, ZyzzyxTech"
 __credits__ = ["Ken W. Alger, Dan Johnson, David Dinkins, Keri Nicole"]
 __license__ = "GPL"
-__version__ = "1.0.1"
+__version__ = "0.1.0"
 __maintainer__ = "Ken W. Alger"
 __email__ = "ken@kenwalger.com"
 __status__ = "Development"
 
+
 import datetime
 
+import bcrypt
 from flask.ext.login import UserMixin
 from peewee import *
 from playhouse.postgres_ext import PostgresqlExtDatabase, JSONField
@@ -32,6 +37,25 @@ class User(UserMixin, BaseModel):
     country = CharField(max_length=25)
     joined_at = DateTimeField(default=datetime.datetime.now)
     is_admin = BooleanField(default=False)
+
+    @classmethod
+    def create_user(cls, username, email, password, github_account_link, city,
+                    state, country, admin=False):
+        # TODO: Generate JSON data prior to storage of user profile data.
+        try:
+            with DATABASE.transaction():
+                cls.create(
+                    th_user_name=username,
+                    email=email,
+                    # Hashes the password for the first time, 12 rounds
+                    password=bcrypt.hashpw(password, bcrypt.gensalt(12)),
+                    github_account_link=github_account_link,
+                    city=city,
+                    state=state,
+                    country=country,
+                    is_admin=admin)
+        except IntegrityError:
+            raise ValueError("Sorry, user already exists.")
 
 
 def initialize():
